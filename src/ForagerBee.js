a9692363e9ea4e814d3164397a78b42934bfd97e
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-11 23:27:11 -0700

    finish 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super()
+    this.age = 10,
+    this.canFly = true,
+    this.treasureChest = [],
+    this.job = 'find pollen'
+  }
+  forage() {this.treasureChest.push(this.treasure)};
+
 };
